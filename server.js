f1e4f2108844cabaec3013fd3b0fb502bde02413
smita1995 <31583642+smita1995@users.noreply.github.com>
2017-09-03 22:36:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one', function (req, res) {
     res.sendFile('Article one requested and will be served here');
 });
 app.get('/article-two', function (req, res) {
-    res.sendFile('Article one requested and will be served here');
+    res.sendFile('Article two requested and will be served here');
 });
 app.get('/article-three', function (req, res) {
-    res.sendFile('Article one requested and will be served here');
+    res.sendFile('Article three requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
