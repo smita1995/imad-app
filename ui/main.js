905e762babcba583278ec8f853c68f9cbcecd01f
smita1995 <31583642+smita1995@users.noreply.github.com>
2017-09-04 15:32:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,17 @@ button.onclick = function () {
     request.open('GET', 'http://smitakhobragade777.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var nameInput = doucument.getElementById('name');
+var name = nameInput.value;
+var submit = doucument.getElementById('submit_btn');
+submit.onclick = function () {
+    
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '<li>';
+    }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
+};
