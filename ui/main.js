41e4ab266cf34163869c4ad0a36f09189cc50832
smita1995 <31583642+smita1995@users.noreply.github.com>
2017-09-04 13:12:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,11 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'New value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft = 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    var interval = setInterval(moveLeft, 100);
-    img.style.marginLeft = '100px';
-};
+    var interval = setInterval(moveRight, 100);
+    };
