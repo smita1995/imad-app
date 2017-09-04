c480dc5de5aacdba0caddbe0488d15b9be180830
smita1995 <31583642+smita1995@users.noreply.github.com>
2017-09-04 13:02:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text'
 );
 element.innerHTML = 'New value';
+
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginleft = '100px';
+};
